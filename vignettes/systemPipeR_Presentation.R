266eae2d2c2abc0b8b9e8a2a25f24c63ad5ca4a9
Thomas Girke <tgirke@citrus.ucr.edu>
2015-07-18 20:30:13 -0700

    some edits

--- vignettes/systemPipeR_Presentation.R
+++ vignettes/systemPipeR_Presentation.R
@@ -59,7 +59,7 @@ sysargs(args)[1]
 ## library(systemPipeR)
 
 ## ----construct_sysargs, eval=FALSE---------------------------------------
-## args <- systemArgs(sysma="trim.param", mytargets="targets.txt")
+## zzargs <- systemArgs(sysma="trim.param", mytargets="targets.txt")
 
 ## ----preprocessing, eval=FALSE-------------------------------------------
 ## preprocessReads(args=args, Fct="trimLRPatterns(Rpattern='GCCCGGGTAA', subject=fq)",
