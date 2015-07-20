45dbcc3db76c669a04b88f5b74dc28a24a249d92
Thomas Girke <tgirke@citrus.ucr.edu>
2015-07-19 18:28:37 -0700

    some edits

--- inst/extdata/workflows/varseq/systemPipeVARseq_single.R
+++ inst/extdata/workflows/varseq/systemPipeVARseq_single.R
@@ -18,6 +18,94 @@ source("systemPipeVARseq_Fct.R")
 targets <- read.delim("targetsPE.txt", comment.char = "#")[,1:5]
 targets
 
+## ----eval=TRUE, cache=TRUE-------------------------------------------------------------------
+args <- systemArgs(sysma="param/bwa.param", mytargets="targets_PEtrim.txt")
+fqlist <- seeFastq(fastq=infile1(args), batchsize=100000, klength=8)
+pdf("./results/fastqReport.pdf", height=18, width=4*length(fqlist))
+seeFastqPlot(fqlist)
+dev.off()
+
+## ----eval=TRUE, cache=TRUE-------------------------------------------------------------------
+library(VariantAnnotation)
+library(BBmisc) # Defines suppressAll()
+args <- systemArgs(sysma="param/filter_gatk.param", mytargets="targets_gatk.txt")[1:4]
+filter <- "totalDepth(vr) >= 2 & (altDepth(vr) / totalDepth(vr) >= 0.8) & rowSums(softFilterMatrix(vr))>=1"
+# filter <- "totalDepth(vr) >= 20 & (altDepth(vr) / totalDepth(vr) >= 0.8) & rowSums(softFilterMatrix(vr))==6"
+suppressAll(filterVars(args, filter, varcaller="gatk", organism="A. thaliana"))
+writeTargetsout(x=args, file="targets_gatk_filtered.txt", overwrite=TRUE)
+
+## ----eval=TRUE, cache=TRUE-------------------------------------------------------------------
+args <- systemArgs(sysma="param/filter_sambcf.param", mytargets="targets_sambcf.txt")[1:4]
+filter <- "rowSums(vr) >= 2 & (rowSums(vr[,3:4])/rowSums(vr[,1:4]) >= 0.8)"
+# filter <- "rowSums(vr) >= 20 & (rowSums(vr[,3:4])/rowSums(vr[,1:4]) >= 0.8)"
+suppressAll(filterVars(args, filter, varcaller="bcftools", organism="A. thaliana"))
+writeTargetsout(x=args, file="targets_sambcf_filtered.txt", overwrite=TRUE)
+
+## ----eval=TRUE, messages=FALSE, warnings=FALSE, cache=TRUE-----------------------------------
+library("GenomicFeatures")
+args <- systemArgs(sysma="param/annotate_vars.param", mytargets="targets_gatk_filtered.txt")
+txdb <- loadDb("./data/tair10.sqlite")
+fa <- FaFile(systemPipeR::reference(args))
+suppressAll(variantReport(args=args, txdb=txdb, fa=fa, organism="A. thaliana"))
+
+## ----eval=TRUE, messages=FALSE, warnings=FALSE, cache=TRUE-----------------------------------
+args <- systemArgs(sysma="param/annotate_vars.param", mytargets="targets_sambcf_filtered.txt")
+txdb <- loadDb("./data/tair10.sqlite")
+fa <- FaFile(systemPipeR::reference(args))
+suppressAll(variantReport(args=args, txdb=txdb, fa=fa, organism="A. thaliana"))
+
+## ----eval=TRUE, messages=FALSE, warnings=FALSE, cache=TRUE-----------------------------------
+args <- systemArgs(sysma="param/annotate_vars.param", mytargets="targets_vartools_filtered.txt")
+txdb <- loadDb("./data/tair10.sqlite")
+fa <- FaFile(systemPipeR::reference(args))
+suppressAll(variantReport(args=args, txdb=txdb, fa=fa, organism="A. thaliana"))
+
+## ----eval=TRUE, messages=FALSE, warnings=FALSE, cache=TRUE-----------------------------------
+read.delim(outpaths(args)[1])[38:40,]
+
+## ----eval=TRUE, cache=TRUE-------------------------------------------------------------------
+args <- systemArgs(sysma="param/annotate_vars.param", mytargets="targets_gatk_filtered.txt")
+combineDF <- combineVarReports(args, filtercol=c(Consequence="nonsynonymous"))
+write.table(combineDF, "./results/combineDF_nonsyn_gatk.xls", quote=FALSE, row.names=FALSE, sep="\t")
+
+## ----eval=TRUE, cache=TRUE-------------------------------------------------------------------
+args <- systemArgs(sysma="param/annotate_vars.param", mytargets="targets_sambcf_filtered.txt")
+combineDF <- combineVarReports(args, filtercol=c(Consequence="nonsynonymous"))
+write.table(combineDF, "./results/combineDF_nonsyn_sambcf.xls", quote=FALSE, row.names=FALSE, sep="\t")
+
+## ----eval=TRUE, cache=TRUE-------------------------------------------------------------------
+args <- systemArgs(sysma="param/annotate_vars.param", mytargets="targets_vartools_filtered.txt")
+combineDF <- combineVarReports(args, filtercol=c(Consequence="nonsynonymous"))
+write.table(combineDF, "./results/combineDF_nonsyn_vartools.xls", quote=FALSE, row.names=FALSE, sep="\t")
+combineDF[2:4,] 
+
+## ----eval=TRUE, cache=TRUE-------------------------------------------------------------------
+args <- systemArgs(sysma="param/annotate_vars.param", mytargets="targets_gatk_filtered.txt")
+write.table(varSummary(args), "./results/variantStats_gatk.xls", quote=FALSE, col.names = NA, sep="\t")
+
+## ----eval=TRUE, cache=TRUE-------------------------------------------------------------------
+args <- systemArgs(sysma="param/annotate_vars.param", mytargets="targets_sambcf_filtered.txt")
+write.table(varSummary(args), "./results/variantStats_sambcf.xls", quote=FALSE, col.names = NA, sep="\t")
+
+## ----eval=TRUE, cache=TRUE-------------------------------------------------------------------
+args <- systemArgs(sysma="param/annotate_vars.param", mytargets="targets_vartools_filtered.txt")
+write.table(varSummary(args), "./results/variantStats_vartools.xls", quote=FALSE, col.names = NA, sep="\t")
+read.delim("results/variantStats_vartools.xls", row.names=1)
+
+## ----eval=TRUE, cache=TRUE-------------------------------------------------------------------
+args <- systemArgs(sysma="param/annotate_vars.param", mytargets="targets_gatk_filtered.txt")
+varlist <- sapply(names(outpaths(args))[1:4], function(x) as.character(read.delim(outpaths(args)[x])$VARID))
+vennset_gatk <- overLapper(varlist, type="vennsets")
+args <- systemArgs(sysma="param/annotate_vars.param", mytargets="targets_sambcf_filtered.txt")
+varlist <- sapply(names(outpaths(args))[1:4], function(x) as.character(read.delim(outpaths(args)[x])$VARID))
+vennset_bcf <- overLapper(varlist, type="vennsets")
+args <- systemArgs(sysma="param/annotate_vars.param", mytargets="targets_vartools_filtered.txt")
+varlist <- sapply(names(outpaths(args))[1:4], function(x) as.character(read.delim(outpaths(args)[x])$VARID))
+vennset_vartools <- overLapper(varlist, type="vennsets")
+pdf("./results/vennplot_var.pdf")
+vennPlot(list(vennset_gatk, vennset_bcf, vennset_vartools), mymain="", mysub="GATK: red; BCFtools: blue; VariantTools: green", colmode=2, ccol=c("red", "blue", "green"))
+dev.off()
+
 ## ----sessionInfo, results='asis'-------------------------------------------------------------
 toLatex(sessionInfo())
 
