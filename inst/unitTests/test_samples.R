bf48a9560a51a0f99aadf2d55a10021b67b9a66d
Thomas Girke <tgirke@citrus.ucr.edu>
2015-08-01 20:49:22 -0700

    some edits

---
+++ inst/unitTests/test_samples.R
@@ -0,0 +1,6 @@
+testDir <- system.file("unitTests", package="systemPipeRdata")
+test.samples <- function(){
+	## Test import whether sample data exist
+    path_list <- pathList()
+    checkTrue(all(file.exists(unlist(path_list))))
+}
