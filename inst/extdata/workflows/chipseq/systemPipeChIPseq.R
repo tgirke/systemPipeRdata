8c3c0b8d34faad290f45640f2806d65c869ac1d0
Thomas Girke <tgirke@citrus.ucr.edu>
2015-08-09 19:28:29 -0700

    chip-seq updates

--- inst/extdata/workflows/chipseq/systemPipeChIPseq.R
+++ inst/extdata/workflows/chipseq/systemPipeChIPseq.R
@@ -66,12 +66,87 @@ targets
 #  writeTargetsout(x=args, file="targets_macs.txt", overwrite=TRUE)
 
 ## ----eval=FALSE------------------------------------------------------------------------------
-#  writeTargetsRef(infile="targets_bam.txt", outfile="targets_bam_ref.txt", silent=FALSE, overwrite=TRUE)
+#  writeTargetsRef(infile="targets_mergeBamByFactor.txt", outfile="targets_bam_ref.txt", silent=FALSE, overwrite=TRUE)
 #  args <- systemArgs(sysma="param/macs2.param", mytargets="targets_bam_ref.txt")
 #  sysargs(args)[1] # Command-line parameters for first FASTQ file
 #  runCommandline(args)
 #  file.exists(outpaths(args))
 #  writeTargetsout(x=args, file="targets_macs.txt", overwrite=TRUE)
+
+## ----eval=FALSE------------------------------------------------------------------------------
+#  library(ChIPpeakAnno); library(GenomicFeatures)
+#  args <- systemArgs(sysma="param/macs2.param", mytargets="targets_bam_ref.txt")
+#  txdb <- loadDb("./data/tair10.sqlite")
+#  ge <- genes(txdb, columns=c("tx_name", "gene_id", "tx_type"))
+#  for(i in seq(along=args)) {
+#      peaksGR <- toGRanges(outpaths(args)[i], format="MACS")
+#      annotatedPeak <- annotatePeakInBatch(peaksGR, AnnotationData=genes(txdb))
+#      df <- data.frame(as.data.frame(annotatedPeak), as.data.frame(values(ge[values(annotatedPeak)$feature,])))
+#      write.table(df, paste0(outpaths(args[i]), ".annotated.xls"), quote=FALSE, row.names=FALSE, sep="\t")
+#  }
+#  ## Perform previous step with full genome annotation from Biomart
+#  # txdb <- makeTxDbFromBiomart(biomart="ENSEMBL_MART_PLANT", dataset="athaliana_eg_gene")
+#  # tx <- transcripts(txdb, columns=c("tx_name", "gene_id", "tx_type"))
+#  # ge <- genes(txdb, columns=c("tx_name", "gene_id", "tx_type")) # works as well
+#  # seqlevels(ge) <- c("Chr1", "Chr2", "Chr3", "Chr4", "Chr5", "ChrC", "ChrM")
+#  # table(mcols(tx)$tx_type)
+#  # tx <- tx[!duplicated(unstrsplit(values(tx)$gene_id, sep=",")) # Keeps only first transcript model for each gene]
+#  # annotatedPeak <- annotatePeakInBatch(macsOutput, AnnotationData = tx)
+
+## ----eval=FALSE------------------------------------------------------------------------------
+#  library(ChIPseeker)
+#  txdb <- loadDb("./data/tair10.sqlite")
+#  for(i in seq(along=args)) {
+#      peakAnno <- annotatePeak(outpaths(args)[i], TxDb=txdb, verbose=FALSE)
+#      df <- as.data.frame(peakAnno)
+#      write.table(df, paste0(outpaths(args[i]), ".annotated2.xls"), quote=FALSE, row.names=FALSE, sep="\t")
+#  }
+
+## ----eval=FALSE------------------------------------------------------------------------------
+#  peak <- readPeakFile(outpaths(args)[1])
+#  covplot(peak, weightCol="X.log10.pvalue.")
+#  peakHeatmap(outpaths(args)[1], TxDb=txdb, upstream=1000, downstream=1000, color="red")
+#  plotAvgProf2(outpaths(args)[1], TxDb=txdb, upstream=1000, downstream=1000, xlab="Genomic Region (5'->3')", ylab = "Read Count Frequency")
+
+## ----eval=FALSE------------------------------------------------------------------------------
+#  library(GenomicRanges)
+#  args_bam <- systemArgs(sysma=NULL, mytargets="targets_bam.txt")
+#  bfl <- BamFileList(outpaths(args_bam), yieldSize=50000, index=character())
+#  args_peak <- systemArgs(sysma="param/macs2_noinput.param", mytargets="targets_mergeBamByFactor.txt")
+#  rangefiles <- outpaths(args_peak)
+#  countDFnames <- countRangeset(bfl, rangefiles, extension="_countDF.xls", mode="Union", ignore.strand=TRUE)
+
+## ----eval=FALSE------------------------------------------------------------------------------
+#  args_bam <- systemArgs(sysma=NULL, mytargets="targets_bam.txt")
+#  cmp <- readComp(file=args_bam, format="matrix")
+#  dbrlist <- runDiff(countfiles=countDFnames, diffFct=run_edgeR, targets=targetsin(args_bam), extension="_edgeR.xls", cmp=cmp[[1]], independent=TRUE, dbrfilter=c(Fold=2, FDR=1))
+
+## ----eval=FALSE------------------------------------------------------------------------------
+#  args <- systemArgs(sysma="param/macs2.param", mytargets="targets_bam_ref.txt")
+#  annofiles <- paste0(outpaths(args), ".annotated.xls"); names(annofiles) <- names(outpaths(args))
+#  gene_ids <- sapply(names(annofiles), function(x) unique(as.character(read.delim(annofiles[x])[,"gene_id"])))
+#  load("data/GO/catdb.RData")
+#  BatchResult <- GOCluster_Report(catdb=catdb, setlist=gene_ids, method="all", id_type="gene", CLSZ=2, cutoff=0.9, gocats=c("MF", "BP", "CC"), recordSpecGO=NULL)
+
+## ----eval=FALSE------------------------------------------------------------------------------
+#  library(Biostrings); library(seqLogo); library(BCRANK)
+#  for(i in seq(along=rangefiles)) {
+#      df <- read.delim(rangefiles[i], comment="#")
+#      peaks <- as(df, "GRanges")
+#      names(peaks) <- paste0(as.character(seqnames(peaks)), "_", start(peaks), "-", end(peaks))
+#      pseq <- getSeq(FaFile("./data/tair10.fasta"), peaks)
+#      names(pseq) <- names(peaks)
+#      writeXStringSet(pseq, paste0(rangefiles[i], ".fasta"))
+#  }
+
+## ----eval=FALSE------------------------------------------------------------------------------
+#  set.seed(0)
+#  BCRANKout <- bcrank(paste0(rangefiles[1], ".fasta"), restarts=25, use.P1=TRUE, use.P2=TRUE)
+#  toptable(BCRANKout)
+#  topMotif <- toptable(BCRANKout, 1)
+#  weightMatrix <- pwm(topMotif, normalize = FALSE)
+#  weightMatrixNormalized <- pwm(topMotif, normalize = TRUE)
+#  seqLogo(weightMatrixNormalized)
 
 ## ----sessionInfo, results='asis'-------------------------------------------------------------
 toLatex(sessionInfo())
