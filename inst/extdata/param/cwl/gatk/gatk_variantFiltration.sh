636409c2d203d7c26fbd4a6d70d239a74ee749d6
lz <lz@DESKTOP-MP3UQ3S.localdomain>
2025-11-24 01:01:09 -0800

    feat: update cwl params for varseq

--- inst/extdata/param/cwl/gatk/gatk_variantFiltration.sh
+++ inst/extdata/param/cwl/gatk/gatk_variantFiltration.sh
@@ -10,8 +10,13 @@ gatk VariantFiltration \
     -V ${input_vcf} \
     -O ${output_vcf} \
     -R ${ref} \
-    -filter "QD < 2.0" --filter-name "QD2" \
-    -filter "QUAL < 30.0" --filter-name "QUAL30" \
-    -filter "SOR > 3.0" --filter-name "SOR3" \
-    -filter "FS > 60.0" --filter-name "FS60" \
-    -filter "MQ < 40.0" --filter-name "MQ40" \
+  	-filter-name "QD_filter" -filter "QD < 2.0" \
+  	-filter-name "FS_filter" -filter "FS > 60.0" \
+  	-filter-name "MQ_filter" -filter "MQ < 40.0" \
+  	-filter-name "SOR_filter" -filter "SOR > 4.0" \
+  	-filter-name "MQRankSum_filter" -filter "MQRankSum < -12.5" \
+  	-filter-name "ReadPosRankSum_filter" -filter "ReadPosRankSum < -8.0" \
+  	-genotype-filter-expression "DP < 10" \
+  	-genotype-filter-name "DP_filter" \
+  	-genotype-filter-expression "GQ < 10" \
+  	-genotype-filter-name "GQ_filter"
